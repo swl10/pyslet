742e4e86e739f68094ed9a0026835b28424fab2e
Steve Lay <steve.w.lay@gmail.com>
2012-05-22 09:49:27 +0000

    Minor changes to fix Common Cartridge parsing of Test Data received in Toronto workshop

--- pyslet/imscc_profilev1p1.py
+++ pyslet/imscc_profilev1p1.py
@@ -18,31 +18,6 @@ IMSCC_LOMMANIFEST_SCHEMALOCATION="http://www.imsglobal.org/profile/cc/ccv1p2/LOM
 IMSCC_LOMRESOURCE_NAMESPACE="http://ltsc.ieee.org/xsd/imsccv1p2/LOM/resource"
 IMSCC_LOMRESOURCE_SCHEMALOCATION="http://www.imsglobal.org/profile/cc/ccv1p2/LOM/ccv1p2_lomresource_v1p0.xsd"
 
-class CCCPDocument(xmlns.XMLNSDocument):
-	classMap={}
-
-	def __init__(self,**args):
-		""""""
-		xmlns.XMLNSDocument.__init__(self,**args)
-		self.defaultNS=IMSCC_CP_NAMESPACE
-		self.MakePrefix(xsi.XMLSCHEMA_NAMESPACE,'xsi')
-		self.MakePrefix(IMSCC_LOMMANIFEST_NAMESPACE,'lomimscc')
-		self.MakePrefix(IMSCC_LOMRESOURCE_NAMESPACE,'lom')
-		schemaLocation=[IMSCC_CP_NAMESPACE,IMSCC_CP_SCHEMALOCATION,
-			IMSCC_LOMMANIFEST_NAMESPACE,IMSCC_LOMMANIFEST_SCHEMALOCATION,
-			IMSCC_LOMRESOURCE_NAMESPACE,IMSCC_LOMRESOURCE_SCHEMALOCATION]
-		if isinstance(self.root,imscp.CPElement):
-			self.root.SetAttribute((xsi.XMLSCHEMA_NAMESPACE,'schemaLocation'),string.join(schemaLocation,' '))
-
-	def GetElementClass(self,name):
-		eClass=CCCPDocument.classMap.get(name,CCCPDocument.classMap.get((name[0],None),xmlns.XMLNSElement))
-		return eClass
-	
-class ContentPackage(imscp.ContentPackage):
-	CPDocumentClass=CCCPDocument
-
-class CommonCartridge(v1p0.CommonCartridge):
-	ContentPackageClass=ContentPackage
 
 class Schema(imscp.Schema):
 	XMLNAME=(IMSCC_CP_NAMESPACE,'schema')
@@ -62,21 +37,61 @@ class Organizations(imscp.Organizations):
 	XMLNAME=(IMSCC_CP_NAMESPACE,'organizations')	
 	OrganizationClass=Organization
 
+class File(imscp.File):
+	XMLNAME=(IMSCC_CP_NAMESPACE,'file')
+
+class Dependency(imscp.Dependency):
+	XMLNAME=(IMSCC_CP_NAMESPACE,'dependency')
+
 class Resource(imscp.Resource):
 	XMLNAME=(IMSCC_CP_NAMESPACE,'resource')
 	MetadataClass=Metadata
+	FileClass=File
+	DependencyClass=Dependency
+
+class Resources(imscp.Resources):
+	XMLNAME=(IMSCC_CP_NAMESPACE,'resources')
+	ResourceClass=Resource
 
 class Manifest(imscp.Manifest):
 	XMLNAME=(IMSCC_CP_NAMESPACE,'manifest')
 	MetadataClass=Metadata
 	OrganizationsClass=Organizations
+	ResourcesClass=Resources
 	ManifestClass=None
 
 Manifest.ManifestClass=Manifest
 
-xmlns.MapClassElements(CCCPDocument.classMap,globals())
-# xmlns.MapClassElements(CCCPDocument.classMap,imsmd)
-# xmlns.MapClassElements(CCCPDocument.classMap,imsqti)
+class ManifestDocument(xmlns.XMLNSDocument):
+	classMap={}
+
+	def __init__(self,**args):
+		""""""
+		xmlns.XMLNSDocument.__init__(self,**args)
+		self.defaultNS=IMSCC_CP_NAMESPACE
+		self.MakePrefix(xsi.XMLSCHEMA_NAMESPACE,'xsi')
+		self.MakePrefix(IMSCC_LOMMANIFEST_NAMESPACE,'lomimscc')
+		self.MakePrefix(IMSCC_LOMRESOURCE_NAMESPACE,'lom')
+		schemaLocation=[IMSCC_CP_NAMESPACE,IMSCC_CP_SCHEMALOCATION,
+			IMSCC_LOMMANIFEST_NAMESPACE,IMSCC_LOMMANIFEST_SCHEMALOCATION,
+			IMSCC_LOMRESOURCE_NAMESPACE,IMSCC_LOMRESOURCE_SCHEMALOCATION]
+		if isinstance(self.root,imscp.CPElement):
+			self.root.SetAttribute((xsi.XMLSCHEMA_NAMESPACE,'schemaLocation'),string.join(schemaLocation,' '))
+
+	def GetElementClass(self,name):
+		eClass=ManifestDocument.classMap.get(name,ManifestDocument.classMap.get((name[0],None),xmlns.XMLNSElement))
+		return eClass
+
+xmlns.MapClassElements(ManifestDocument.classMap,globals())
+# xmlns.MapClassElements(ManifestDocument.classMap,imsmd)
+# xmlns.MapClassElements(ManifestDocument.classMap,imsqti)
 # Add other supported metadata schemas in here
+	
+class ContentPackage(imscp.ContentPackage):
+	ManifestDocumentClass=ManifestDocument
+
+class CommonCartridge(v1p0.CommonCartridge):
+	ContentPackageClass=ContentPackage
+
 
 
