48714922b4fa814b4a52f9a6bf78d2271856236c
Steve Lay <steve.w.lay@gmail.com>
2014-05-28 09:22:37 +0100

    Changes for PEP-8 #3

--- pyslet/info.py
+++ pyslet/info.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
 """The module creates some basic constants to describe the Pyslet package."""
 
-title_name=u"Pyslet"
-name="pyslet"
-copyright=u"\xA92008-2014, Steve Lay"
+title_name = u"Pyslet"
+name = "pyslet"
+copyright = u"\xA92008-2014, Steve Lay"
 
-major_version="0.4"
-version="%s.20140528d1"%major_version
+major_version = "0.4"
+version = "%s.20140528d1" % major_version
 
-title="Pyslet: python package for Standards in Learning, Education and Training"
-home="http://www.pyslet.org/"
+title = (
+    "Pyslet: "
+    "Python package for Standards in Learning, Education and Training")
 
-
-
+home = "http://www.pyslet.org/"
