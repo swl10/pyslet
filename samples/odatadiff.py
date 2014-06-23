ce85f3894025d9feddb758073dc445d542c4f483
Steve Lay <steve.w.lay@gmail.com>
2014-06-23 14:21:39 +0100

    #7 new odata cmd line tools
    Adds odatadiff.py for comparing old and new versions of the same OData
    service (useful during development) and odatadoc.py which generates an
    xhtml documentation file from an OData metadata document.

---
+++ samples/odatadiff.py
@@ -0,0 +1,239 @@
+#! /usr/bin/env python
+"""Compares two OData metadata documents"""
+
+import sys
+import os
+import io
+import string
+import logging
+import traceback
+from optparse import OptionParser
+import getpass
+
+import pyslet.rfc2396 as uri
+import pyslet.rfc2617 as auth
+import pyslet.rfc2616 as http
+import pyslet.odata2.csdl as edm
+import pyslet.odata2.metadata as edmx
+
+
+def fetch_url(url, username=None, password=None):
+    mgr = http.HTTPRequestManager()
+    url = uri.URIFactory.URI(url)
+    # does url end with $metadata?
+    if url.GetFileName() != "$metadata":
+        url = uri.URIFactory.Resolve(url, "$metadata")
+    if username:
+        cred = auth.BasicCredentials()
+        cred.userid = username
+        cred.password = password
+        cred.protectionSpace = url.GetCanonicalRoot()
+        mgr.AddCredentials(cred)
+    doc = edmx.Document(baseURI=url, reqManager=mgr)
+    doc.Read()
+    mgr.close()
+    if not doc.root.GetBase():
+        doc.root.SetBase(url)
+    return doc
+
+
+def load_file(filename):
+    doc = edmx.Document()
+    with open(filename, 'rb') as f:
+        doc.Read(f)
+    return doc
+
+
+def save_file(doc, filename):
+    # check the base of the doc's root element
+    with io.FileIO(filename, "w") as f:
+        with io.BufferedWriter(f) as bf:
+            with io.TextIOWrapper(bf, encoding="utf-8") as tf:
+                doc.Create(tf)
+
+
+def splitlists(old_list, new_list):
+    """Compares lists of strings
+
+    Returns three lists representing strings that have stayed the same,
+    strings that have been added in *new_list* and finally strings that
+    have been removed from *old_list*."""
+    snew = set(new_list)
+    sold = set(old_list)
+    added = []
+    removed = []
+    common = []
+    for newitem in new_list:
+        if newitem in sold:
+            common.append(newitem)
+        else:
+            added.append(newitem)
+    for olditem in old_list:
+        if olditem not in snew:
+            removed.append(olditem)
+    return common, added, removed
+
+
+def odatadiff(old_doc, new_doc, backlinks=False):
+    if not isinstance(old_doc.root, edmx.Edmx):
+        return "File was not a DataServices document - remove it and re-run"
+    if not isinstance(new_doc.root, edmx.Edmx):
+        return "No DataServices document found - check service URL"
+    oldds = old_doc.root.DataServices
+    newds = new_doc.root.DataServices
+    oldschemas = map(lambda x: x.name, oldds.Schema)
+    newschemas = map(lambda x: x.name, newds.Schema)
+    common, added, removed = splitlists(oldschemas, newschemas)
+    for name in added:
+        logging.info("%s new Schema Namespace", name)
+    for name in removed:
+        logging.info("%s Schema Namespace removed", name)
+    for name in common:
+        logging.debug("%s checking Schema...", name)
+        schemadiff(oldds[name], newds[name], backlinks=backlinks)
+        logging.debug("%s done", name)
+
+
+def schemadiff(oldds, newds, backlinks=False):
+    oldtypes = map(lambda x: x.name, oldds.EntityType)
+    newtypes = map(lambda x: x.name, newds.EntityType)
+    common, added, removed = splitlists(oldtypes, newtypes)
+    for name in added:
+        logging.info("%s.%s new EntityType", oldds.name, name)
+    for name in removed:
+        logging.info("%s.%s EntityType removed", oldds.name, name)
+    for name in common:
+        logging.debug("%s.%s checking EntityType...", oldds.name, name)
+        entitydiff(oldds[name], newds[name], backlinks=backlinks)
+        logging.debug("%s done", name)
+#     oldassoc = map(lambda x: x.name, oldds.Association)
+#     newassoc = map(lambda x:x.name, newds.Association)
+#     common, added, removed = splitlists(oldassoc, newassoc)
+#     for name in added:
+#         logging.info("%s.%s new Association",oldds.name,name)
+#     for name in removed:
+#         logging.info("%s.%s Association removed",oldds.name,name)
+#     for name in common:
+#         logging.debug("%s.%s checking Association...",oldds.name,name)
+
+
+def entitydiff(oldet, newet, backlinks=False):
+    oldprops = map(lambda x: x.name, oldet.Property)
+    newprops = map(lambda x: x.name, newet.Property)
+    common, added, removed = splitlists(oldprops, newprops)
+    for name in added:
+        logging.info("%s.%s new Property", oldet.name, name)
+    for name in removed:
+        logging.info("%s.%s Property removed", oldet.name, name)
+    for name in common:
+        logging.debug("%s.%s checking Property...", oldet.name, name)
+        propdiff(oldet[name], newet[name])
+        logging.debug("%s done", name)
+    oldkeys = map(lambda x: x.name, oldet.Key.PropertyRef)
+    newkeys = map(lambda x: x.name, newet.Key.PropertyRef)
+    common, added, removed = splitlists(oldkeys, newkeys)
+    for name in added:
+        logging.info("%s.%s new Key Property", oldet.name, name)
+    for name in removed:
+        logging.info("%s.%s Key Property removed", oldet.name, name)
+    oldnav = map(lambda x: x.name, oldet.NavigationProperty)
+    newnav = map(lambda x: x.name, newet.NavigationProperty)
+    common, added, removed = splitlists(oldnav, newnav)
+    for name in added:
+        logging.info("%s.%s new NavigationProperty", oldet.name, name)
+    for name in removed:
+        logging.info("%s.%s NavigationProperty removed", oldet.name, name)
+    for name in common:
+        logging.debug("%s.%s checking NavigationProperty...", oldet.name, name)
+        navdiff(oldet[name], newet[name], backlinks=backlinks)
+        logging.debug("%s done", name)
+
+
+def propdiff(oldp, newp):
+    if oldp.type != newp.type:
+        logging.info("%s.%s Property type changed from %s to %s",
+                     oldp.parent.name, oldp.name, oldp.type, newp.type)
+    if oldp.nullable != newp.nullable:
+        logging.info("%s.%s Property nullable changed from %s to %s",
+                     oldp.parent.name, oldp.name,
+                     str(oldp.nullable), str(newp.nullable))
+
+
+def navdiff(oldnp, newnp, backlinks=False):
+    if oldnp.toEnd.type != newnp.toEnd.type:
+        logging.info("%s.%s Navigation target type changed from %s to %s",
+                     oldnp.parent.name, oldnp.name,
+                     oldnp.toEnd.type, newnp.toEnd.type)
+    if oldnp.toEnd.multiplicity != newnp.toEnd.multiplicity:
+        logging.info("%s.%s Navigation target multiplicity changed "
+                     "from %s to %s",
+                     oldnp.parent.name, oldnp.name,
+                     edm.EncodeMultiplicity(oldnp.toEnd.multiplicity),
+                     edm.EncodeMultiplicity(newnp.toEnd.multiplicity))
+    if backlinks:
+        old_backname = new_backname = None
+        if oldnp.backLink is not None:
+            old_backname = oldnp.backLink.name
+        if newnp.backLink is not None:
+            new_backname = oldnp.backLink.name
+        if old_backname != new_backname:
+            logging.info("%s.%s Navigation back link changed from %s to %s",
+                         oldnp.parent.name, oldnp.name,
+                         old_backname, new_backname)
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-s", "--service", dest="url",
+                      help="fetch metadata from URL for diff")
+    parser.add_option("-u", "--user", dest="user",
+                      help="user name for basic auth credentials")
+    parser.add_option("--password", dest="password",
+                      help="password for basic auth credentials")
+    parser.add_option("-v", action="count", dest="logging",
+                      default=0, help="increase verbosity of output")
+    parser.add_option("-b", "--backlinks", action="store_true",
+                      dest="backlinks", default=False,
+                      help="check navigation backlinks")
+    (options, args) = parser.parse_args()
+    if options.logging > 3:
+        level = 3
+    else:
+        level = options.logging
+    logging.basicConfig(level=[logging.ERROR, logging.WARN, logging.INFO,
+                        logging.DEBUG][level])
+    if len(args) != 1:
+        sys.exit("Usage: odatadiff.py [metadata.xml]")
+    if options.user is not None:
+        username = options.user
+        if options.password is not None:
+            password = options.password
+        else:
+            password = getpass.getpass()
+    else:
+        username = password = None
+    try:
+        if os.path.exists(args[0]):
+            # called with an existing file, load it
+            old_doc = load_file(args[0])
+            if options.url is None:
+                # use the base of old_doc
+                url = old_doc.root.GetBase()
+                if url is None:
+                    sys.exit("xml:base undefined, try again with -s SERVICE")
+                new_doc = fetch_url(url, username, password)
+            else:
+                new_doc = fetch_url(options.url, username, password)
+            sys.exit(odatadiff(old_doc, new_doc,
+                               backlinks=options.backlinks))
+        else:
+            # file does not exist, just fetch the metadata
+            if options.url is None:
+                sys.exit("%s does not exist, use --s URL "
+                         "to fetch metadata for the first time" % args[0])
+            old_doc = fetch_url(options.url, username, password)
+            save_file(old_doc, args[0])
+    except Exception, e:
+        logging.debug(string.join(traceback.format_exception(*sys.exc_info(),
+                                                             limit=10)))
+        print e
---
+++ samples/odatadoc.py
@@ -0,0 +1,234 @@
+#! /usr/bin/env python
+"""Generats an HTML document from an OData metadata document"""
+
+
+import sys
+import string
+import logging
+from optparse import OptionParser
+import getpass
+
+import pyslet.iso8601 as iso
+import pyslet.rfc2396 as uri
+import pyslet.rfc2617 as auth
+import pyslet.rfc2616 as http
+import pyslet.odata2.csdl as edm
+import pyslet.odata2.metadata as edmx
+import pyslet.xml20081126.structures as xml
+
+
+def fetch_url(url, username=None, password=None):
+    mgr = http.HTTPRequestManager()
+    url = uri.URIFactory.URI(url)
+    # does url end with $metadata?
+    if url.GetFileName() != "$metadata":
+        url = uri.URIFactory.Resolve(url, "$metadata")
+    if username:
+        cred = auth.BasicCredentials()
+        cred.userid = username
+        cred.password = password
+        cred.protectionSpace = url.GetCanonicalRoot()
+        mgr.AddCredentials(cred)
+    doc = edmx.Document(baseURI=url, reqManager=mgr)
+    doc.Read()
+    mgr.close()
+    if not doc.root.GetBase():
+        doc.root.SetBase(url)
+    return doc
+
+
+def load_file(filename):
+    doc = edmx.Document()
+    with open(filename, 'rb') as f:
+        doc.Read(f)
+    return doc
+
+
+def write_doc(doc, template, out):
+    if not isinstance(doc.root, edmx.Edmx):
+        return "Source was not a DataServices document"
+    with open(template, 'rb') as f:
+        data = f.read()
+    params = {
+        'namespace': "",
+        'summary': "Schema Documentation",
+        'description': '',
+        'entity_list': "<p>Not supported in this version</p>",
+        'tables': "<p>Not supported in this version</p>",
+        'index': "<p>Not supported in this version</p>",
+        'date': str(iso.TimePoint.FromNow())
+    }
+    ds = doc.root.DataServices
+    if len(ds.Schema) != 1:
+        logging.warn("Documenting the first Schema tag only")
+    params['namespace'] = xml.EscapeCharData7(ds.Schema[0].name)
+    sdoc = ds.Schema[0].Documentation
+    if sdoc is not None:
+        if sdoc.Summary is not None:
+            params['summary'] = "%s" % xml.EscapeCharData7(
+                sdoc.Summary.GetValue())
+        if sdoc.LongDescription is not None:
+            params['description'] = "%s" % xml.EscapeCharData7(
+                sdoc.LongDescription.GetValue())
+    tables = []
+    dl_items = []
+    index_items = []
+    for ec in ds.Schema[0].EntityContainer:
+        if not ec.IsDefaultEntityContainer():
+            logging.warn("Ignoring non-default EntityContainer: %s", ec.name)
+            continue
+        es_list = []
+        for es in ec.EntitySet:
+            es_list.append(es.name)
+        es_list.sort()
+        for esn in es_list:
+            es = ec[esn]
+            dl_items.append('<dt><a href=%s>%s</a></dt>' %
+                            (xml.EscapeCharData7("#" + es.name, True),
+                             xml.EscapeCharData7(es.name)))
+            index_items.append((es.name, es.name, "entity set"))
+            if es.Documentation is not None:
+                if es.Documentation.Summary is not None:
+                    dl_items.append('<dd>%s</dd>' % xml.EscapeCharData7(
+                        es.Documentation.Summary.GetValue()))
+            tables.append(es_table(es, index_items))
+    params['entity_list'] = string.join(dl_items, '\n')
+    params['tables'] = string.join(tables, "\n\n")
+    index_items.sort()
+    index_dl = []
+    cname = ''
+    for name, link, note in index_items:
+        if name != cname:
+            index_dl.append('<dt>%s</dt>' % xml.EscapeCharData7(name))
+            cname = name
+        index_dl.append('<dd><a href=%s>%s</a></dd>' %
+                        (xml.EscapeCharData7("#" + link, True),
+                         xml.EscapeCharData7(note)))
+    params['index'] = string.join(index_dl, '\n')
+    out.write(data % params)
+    return 0
+
+
+def es_table(es, index_items):
+    result = """<h3><a id=%(anchor)s>%(title)s</a></h3>
+%(summary)s
+%(description)s
+<table class="typedef">
+    <thead>
+        <th>Name</th>
+        <th>Type</th>
+        <th>Multiplicity</th>
+        <th>Description</th>
+        <th>Notes</th>
+    </thead>
+    <tbody>%(body)s</tbody>
+</table>"""
+    params = {
+        'anchor': xml.EscapeCharData7(es.name, True),
+        'title': '',
+        'summary': '',
+        'description': '',
+        'body': ''}
+    tb = []
+    type = es.entityType
+    if type.HasStream():
+        params['title'] = (xml.EscapeCharData7(es.name) +
+                           " <em>(Media Resource)</em>")
+    else:
+        params['title'] = xml.EscapeCharData7(es.name)
+    typedoc = type.Documentation
+    if typedoc is not None:
+        if typedoc.Summary is not None:
+            params['summary'] = (
+                '<p class="summary">%s</p>' %
+                xml.EscapeCharData7(typedoc.Summary.GetValue()))
+        if typedoc.LongDescription is not None:
+            params['description'] = (
+                '<p class="description">%s</p>' %
+                xml.EscapeCharData7(typedoc.LongDescription.GetValue()))
+    for p in type.Property:
+        if p.name in es.keys:
+            tr = ['<tr class="key">']
+        else:
+            tr = ["<tr>"]
+        link = '%s.%s' % (es.name, p.name)
+        tr.append("<td><a id=%s>%s</a></td>" % (
+            xml.EscapeCharData(link, True),
+            xml.EscapeCharData7(p.name)))
+        index_items.append((p.name, link, "property of %s" % es.name))
+        tr.append("<td>%s</td>" % xml.EscapeCharData7(p.type))
+        tr.append("<td>%s</td>" % ("Optional" if p.nullable else "Required"))
+        summary = description = ""
+        if p.Documentation is not None:
+            if p.Documentation.Summary:
+                summary = p.Documentation.Summary.GetValue()
+            if p.Documentation.LongDescription:
+                description = p.Documentation.LongDescription.GetValue()
+        tr.append("<td>%s</td>" % xml.EscapeCharData7(summary))
+        tr.append("<td>%s</td>" % xml.EscapeCharData7(description))
+        tr.append("</tr>")
+        tb.append(string.join(tr, ''))
+    for np in type.NavigationProperty:
+        tr = ['<tr class="navigation">']
+        link = '%s.%s' % (es.name, np.name)
+        tr.append("<td><a id=%s>%s</a></td>" % (
+            xml.EscapeCharData(link, True),
+            xml.EscapeCharData7(np.name)))
+        index_items.append((np.name, link, "navigation property of %s" %
+                            es.name))
+        tr.append("<td><em>%s</em></td>" %
+                  xml.EscapeCharData7(es.NavigationTarget(np.name).name))
+        tr.append("<td>%s</td>" %
+                  edm.EncodeMultiplicity(np.toEnd.multiplicity))
+        summary = description = ""
+        if np.Documentation is not None:
+            if np.Documentation.Summary:
+                summary = np.Documentation.Summary.GetValue()
+            if np.Documentation.LongDescription:
+                description = np.Documentation.LongDescription.GetValue()
+        tr.append("<td>%s</td>" % xml.EscapeCharData7(summary))
+        tr.append("<td>%s</td>" % xml.EscapeCharData7(description))
+        tr.append("</tr>")
+        tb.append(string.join(tr, ''))
+
+    params['body'] = string.join(tb, '\n')
+    return result % params
+
+
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-s", "--service", dest="url",
+                      help="fetch metadata from URL for diff")
+    parser.add_option("-u", "--user", dest="user",
+                      help="user name for basic auth credentials")
+    parser.add_option("--password", dest="password",
+                      help="password for basic auth credentials")
+    parser.add_option("-t", "--template", dest="template",
+                      default="odatadoc.tmpl",
+                      help="path to the template file")
+    parser.add_option("-v", action="count", dest="logging",
+                      default=0, help="increase verbosity of output")
+    (options, args) = parser.parse_args()
+    if options.logging > 3:
+        level = 3
+    else:
+        level = options.logging
+    logging.basicConfig(
+        level=[logging.ERROR, logging.WARN, logging.INFO,
+               logging.DEBUG][level])
+    if len(args) != 1 and options.url is None:
+        sys.exit("Usage: odatadoc.py [-s URL] | [metadata]")
+    if options.user is not None:
+        username = options.user
+        if options.password is not None:
+            password = options.password
+        else:
+            password = getpass.getpass()
+    else:
+        username = password = None
+    if not args:
+        # load metadata from the URL
+        doc = fetch_url(options.url, username, password)
+    else:
+        doc = load_file(args[0])
+    sys.exit(write_doc(doc, options.template, sys.stdout))
