92a8be1132d7f9baf9b473e636e3af5cf88d3ce8
Steve Lay <steve.w.lay@gmail.com>
2014-08-01 00:42:06 +0100

    #10 metadata for Python 2.6

--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ else:
             'Operating System :: OS Independent',
             'Programming Language :: Python',
             'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
             'Topic :: Education',
             'Topic :: Education :: Computer Aided Instruction (CAI)',
