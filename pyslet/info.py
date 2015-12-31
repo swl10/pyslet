c674b485120e8c46279b1955ca5b6d1d2628cde6
Steve Lay <steve.w.lay@gmail.com>
2015-12-31 09:44:01 +0000

    Version bump after merge of #36 and #37

--- pyslet/info.py
+++ pyslet/info.py
@@ -6,7 +6,7 @@ name = "pyslet"
 copyright = u"\xA92008-2014, Steve Lay"
 
 major_version = "0.5"
-build_date = "20150719"
+build_date = "20151231"
 version = "%s.%s" % (major_version, build_date)
 
 title = (
